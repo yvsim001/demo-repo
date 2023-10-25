c3b714fc56fbd97ff265a4d2d6390904883f1ece
Yvan Simo <107270125+yvsim001@users.noreply.github.com>
2023-10-25 14:26:12 +0200

    main.cpp modifiert

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
-#include <iostream>
+#include <avr/io.h>
+
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <util/delay.h>
+#include <string.h>
